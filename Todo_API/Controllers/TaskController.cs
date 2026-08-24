7bd1e4e609b01a6b890a95ece9518bf24ab88d02
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2026-08-24 14:13:59 +0200

    updte

--- Todo_API/Controllers/TaskController.cs
+++ Todo_API/Controllers/TaskController.cs
@@ -14,14 +14,17 @@ namespace Todo_API.Controllers
             _dbContext = dbContext;
         }
 
+        //This will get all the tasks
         [HttpGet]
         public IActionResult GetAllTasks()
         {
             var tasks = _dbContext.TaskItems.ToList(); 
             return Ok(tasks);
         }
-        
+
+
         // GET one specific task
+        //api/task/5 
         [HttpGet("{id}")]
         public IActionResult GetTask(Guid id)
         {
@@ -33,30 +36,70 @@ namespace Todo_API.Controllers
             return Ok(task);
         }
 
-        // POST - create a new task
-        [HttpPost]
-        public IActionResult CreateTask([FromBody] TaskItem newTask)
-        {
-            newTask.Id = Guid.NewGuid();
-            _dbContext.TaskItems.Add(newTask);
-            _dbContext.SaveChanges();
-            return Ok(newTask);
-        }
 
-        // DELETE - remove a task
-        [HttpDelete("{id}")]
-        public IActionResult DeleteTask(Guid id)
-        {
-            var task = _dbContext.TaskItems.FirstOrDefault(t => t.Id == id);
-            if (task == null)
-            {
-                return NotFound();
-            }
 
-            _dbContext.TaskItems.Remove(task);
-            _dbContext.SaveChanges();
 
-            return NoContent();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //    // POST - create a new task
+        //    /*[FromBody] explicitly says that the content will be sent from the request body 
+        //     The request body will be an object of the TaskItem class */
+        //    [HttpPost]
+        //    public IActionResult CreateTask([FromBody] TaskItem newTask)
+        //    {
+        //        newTask.Id = Guid.NewGuid();
+        //        _dbContext.TaskItems.Add(newTask);
+        //        _dbContext.SaveChanges();
+        //        return Ok(newTask); //for now lets use Ok() which means success but you can also be explicit
+        //    }
+
+        //    //PUT - update an existing task, needs an Id extension in the URL 
+        //    [HttpPut("{id}")]
+        //    public IActionResult UpdateTask(Guid id, [FromBody] bool isComplete)
+        //    {
+        //        var task = _dbContext.TaskItems.FirstOrDefault(t => t.Id == id);
+        //        if (task == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        task.IsComplete = isComplete;
+        //        _dbContext.SaveChanges();
+
+        //        return Ok(task);
+        //    }
+
+        //    // DELETE - remove a task
+        //    [HttpDelete("{id}")]
+        //    public IActionResult DeleteTask(Guid id)
+        //    {
+        //        var task = _dbContext.TaskItems.FirstOrDefault(t => t.Id == id);
+        //        if (task == null)
+        //        {
+        //            return NotFound();
+        //        }
+
+        //        _dbContext.TaskItems.Remove(task);
+        //        _dbContext.SaveChanges();
+
+        //        return NoContent();
+        //    }
         }
     }
-}
